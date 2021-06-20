d19b9beb3d0ec5dd366c9b39fd0dfb7ae77c7bcb
GameDevTK <gamedevkannotatsuki@gmail.com>
2021-06-21 07:14:32 +0900

    配列について

--- Argorithm/Argorithm/main.cpp
+++ Argorithm/Argorithm/main.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define N 5
+
+int main()
+{
+	int a[N];
+	
+	for (int i = 0; i < N; i++)
+	{
+		printf("a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+
+	puts("各要素の値");
+	for (int i = 0; i < N; i++)
+	{
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+
+	return 0;
+}

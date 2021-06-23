bdc887c3a961754b7e9624a983ec8a78b55f6474
GameDevTK <gamedevkannotatsuki@gmail.com>
2021-06-24 07:36:52 +0900

    sample.h/cppの追加

--- Argorithm/Argorithm/main.cpp
+++ Argorithm/Argorithm/main.cpp
@@ -1,23 +1,8 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-#define N 5
+﻿#include "sample.h"
 
 int main()
 {
-	int a[N];
-	
-	for (int i = 0; i < N; i++)
-	{
-		printf("a[%d] : ", i);
-		scanf("%d", &a[i]);
-	}
-
-	puts("各要素の値");
-	for (int i = 0; i < N; i++)
-	{
-		printf("a[%d] = %d\n", i, a[i]);
-	}
-
+	setupArraySample00();
 	return 0;
 }
+
---
+++ Argorithm/Argorithm/sample.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define N 5
+
+void setupArraySample00()
+{
+	int a[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+
+	puts("各要素の値");
+	for (int i = 0; i < N; i++)
+	{
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+
+}

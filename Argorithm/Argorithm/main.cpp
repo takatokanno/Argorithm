94d0f0311d863337a5e82f2efcfc509696f6889a
takatokanno <takatokannogamedev@gmail.com>
2021-07-04 11:21:12 +0900

    乱数の生成

--- Argorithm/Argorithm/main.cpp
+++ Argorithm/Argorithm/main.cpp
@@ -1,12 +1,34 @@
-﻿#include "sample.h"
-#include <stdlib.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "sample.h"
+
 
 int main()
 {
-	
+	int i;
+	int* height;
+	int number;
 
+	printf("人数：");
+	scanf("%d", &number);
+
+	height = (int*)calloc(number, sizeof(int));
+
+	srand(time(NULL));
+	for (i = 0; i < number; i++)
+	{
+		height[i] = 100 + rand() % 90;
+		printf("height[%d] =%d\n", i, height[i]);
+	}
+
+	printf("最大値は%dです。\n", maxof(height, number));
+
+
+	free(height);
 
 
 	return 0;
 }
+
--- Argorithm/Argorithm/sample.cpp
+++ Argorithm/Argorithm/sample.cpp
@@ -11,7 +11,7 @@ void setupArraySample00()
 	for (int i = 0; i < N; i++)
 	{
 		printf("a[%d] : ", i);
-		scanf("%d", &a[i]);
+		int b = scanf("%d", &a[i]);
 	}
 
 	puts("各要素の値");
@@ -57,8 +57,45 @@ void dynamicMemoryAllocation()
 	else {
 		*x = 10;
 		printf("*x =  %d\n", *x);
-		printf("x =  %d\n", x);
 		free(x);						// int型のオブジェクトを破棄
 	}
 
 }
+
+//------------------------------------------------------------------------------
+// 配列を動的生成するサンプル
+//------------------------------------------------------------------------------
+void dynamicArraySample()
+{
+	int i;
+	int* a;	// 配列の先頭要素
+	int na;	// 配列aの要素数
+
+	printf("要素数：");
+	scanf("%d", &na);
+
+	a = (int*)calloc(na, sizeof(int)); // 要素数naのint型配列を生成
+	//a = (int*)malloc(na*sizeof(int)); // 要素数naのint型配列を生成
+	//int* b = (int*)realloc(a, na * sizeof(int)*100);
+
+	if (a == NULL)
+	{
+		puts("記憶域の確保に失敗しました");
+	}
+	else {
+		printf("%d個の整数を入力してください。\n", na);
+		for (i = 0; i < na; i++) {
+			printf("a[%d]:", i);
+			scanf("%d", &a[i]);
+		}
+
+		printf("各要素の値は以下のとおりです。\n");
+
+		for (i = 0; i < na; i++) {
+			printf("a[%d] = %d\n", i, a[i]);
+		}
+		free(a);//要素数naのint型配列を破棄
+	}
+}
+
+
